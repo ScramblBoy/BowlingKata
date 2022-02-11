cd56780f94ccc097ec814fc4cf7de6c80043c11a
ScramblBoy <marlon0205@yahoo.de>
2022-02-11 02:15:05 +0100

    added function and tests for calculating the score. TODO: solve corner case of a game of strikes only

--- functional_approach.py
+++ functional_approach.py
@@ -1,17 +1,15 @@
 def is_valid_sequence(sequence):
     if len(sequence) == 22:
-        if sequence[-4] == 10:
-            return are_all_frames_valid(sequence)
+        if sequence[-4] == 10:  # checks if last regular frame is a strike
+            if sequence[-2] == 10:  # checks if first roll after last frame is also a strike
+                return sequence[-1] in range(0, 11) and are_all_frames_valid(sequence[:-2])
+            else:
+                return are_all_frames_valid(sequence)
         else:
             return False
     elif len(sequence) == 21:
-        if sequence[-3] + sequence[-2] == 10:
-            if sequence[-1] in range(0, 11):
-                return are_all_frames_valid(sequence[:-1])
-            else:
-                return False
-        else:
-            return False
+        return sequence[-3] + sequence[-2] == 10 and sequence[-1] in range(0, 11) and are_all_frames_valid(
+            sequence[:-1])
     elif len(sequence) == 20:
         return are_all_frames_valid(sequence)
     else:
@@ -25,5 +23,20 @@ def are_all_frames_valid(sequence):
     return True
 
 
-if __name__ == '__main__':
-    print("hello world")
+def calculate_score(sequence):
+    framescores = []
+    if is_valid_sequence(sequence):
+        for counter in range(0, 20, 2):
+            if sequence[counter] == 10:
+                if sequence[counter + 2] == 10:
+                    framescores.append(20 + sequence[counter + 4])
+                else:
+                    framescores.append(10 + sum(sequence[counter + 2:counter + 4]))
+            elif sum(sequence[counter:counter + 2]) == 10:
+                framescores.append(10 + sequence[counter + 2])
+            else:
+                framescores.append(sum(sequence[counter:counter + 2]))
+        return sum(framescores)
+    else:
+        print("invalid sequence!")
+        return
--- tests/test_functional_approach.py
+++ tests/test_functional_approach.py
@@ -10,3 +10,10 @@ def test_are_all_frames_valid():
 def test_is_valid_sequence():
     assert not functional_approach.is_valid_sequence([10, 0])
     assert not functional_approach.is_valid_sequence([1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1])
+
+
+def test_calculate_score():
+    assert functional_approach.calculate_score([9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0]) == 90
+    assert functional_approach.calculate_score([5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]) == 150
+    assert functional_approach.calculate_score(
+        [10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 10]) == 300
